42f3f6ddaca4d1f8b2c3ce1cc01dc5f70630f6e3
Sergey <ser12563208@yandex.ru>
2020-05-06 20:26:53 +0400

    Start commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <x-container>
+    <div>
+      <h1 class="title lg:mb-10">
+        vx_framework
+      </h1>
+      <XCard>
+        <h2 class="subtitle">
+          site creating framework
+        </h2>
+      </XCard>
+    </div>
+  </x-container>
+</template>
+
+<script>
+import XContainer from '@/UI/src/components/XContainer/XContainer'
+export default {
+  components: { XContainer },
+  async asyncData(contenxt) {
+    const pages = await contenxt.$axios.get(
+      'https://73kprf.ru/wp-json/wp/v2/news'
+    )
+    // console.log(pages.data)
+    return { pages: pages.data }
+  },
+}
+</script>
+
+<style></style>
